bdf0f52bf09e63e9f3e6a278cb06bea60435d405
RAKIBUL <rakibulmd933@gmail.com>
2023-01-29 21:11:48 +0600

    [ADD] assets folder add

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -4,9 +4,9 @@ import Navbar from '../Shared/Navbar/Navbar';
 
 const NotFound = () => {
 	return (
-		<div className='h-screen '>
+		<div>
 			<Navbar />
-			<div className='py-4 my-6 text-center'>
+			<div className='my-2 text-center'>
 				<p className='font-extrabold text-red-600 text-8xl'>4 <span className='text-blue-800 '>0</span> 4</p>
 				<p className='text-red-500'>Something went wrong!!!</p>
 				<h4 className="text-3xl"> Please <button>Sign out</button> and log back in</h4>
