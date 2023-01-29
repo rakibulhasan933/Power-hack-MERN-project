dcaf37c89c88c33cdf2ea9eff6a2dc98d91cf601
RAKIBUL <rakibulmd933@gmail.com>
2023-01-29 23:31:09 +0600

    [ADD] payment list searchbar

--- src/Pages/PaymentList/PaymentList.js
+++ src/Pages/PaymentList/PaymentList.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 const PaymentList = () => {
 	return (
-		<div>
-			Payment List
+		<div >
+			<div className="flex flex-col ">
+				<div className="flex flex-row p-3 my-2 rounded-sm bg-base-200">
+					<div className="flex justify-start">
+						<h2>Billing</h2>
+					</div>
+					<div className="flex justify-center ">
+						<input type="text" placeholder="Search" className="input input-bordered" />
+					</div>
+					<div className="flex justify-end">
+						<button>Add Bill</button>
+					</div>
+				</div>
+				<div className="">
+					table
+				</div>
+			</div>
 		</div>
 	);
 };
