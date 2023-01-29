a2bc016f2d3fcd88058f83c34c301213630ac840
RAKIBUL <rakibulmd933@gmail.com>
2023-01-29 20:20:36 +0600

    [ADD] navbar added

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
 	return (
-		<div className='flex flex-col justify-center gap-2 '>
+		<div className='my-3 text-center'>
 			<p className='text-red-500'>Something went wrong!!!</p>
 			<p className='text-red-400'>Error Message</p>
 			<h4 className="text-3xl"> Please <button>Sign out</button> and log back in</h4>
