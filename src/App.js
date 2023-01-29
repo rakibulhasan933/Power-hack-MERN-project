5e5e54c8f9d8cb0b6b3e24b989717e2e7a182536
RAKIBUL <rakibulmd933@gmail.com>
2023-01-29 20:15:51 +0600

    [ADD] layout notfound home page set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Router/Routes/Routes';
 
 function App() {
   return (
-    <div>
-      Hello World
+    <div className='max-w-[1440px] mx-auto'>
+      <RouterProvider router={router} />
     </div>
   );
 }
