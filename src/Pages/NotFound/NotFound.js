7555ea1487d45b8020c8743aae519a9a75dcb9d1
RAKIBUL <rakibulmd933@gmail.com>
2023-01-29 20:35:45 +0600

    [ADD] footer add

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Footer from '../Shared/Footer/Footer';
+import Navbar from '../Shared/Navbar/Navbar';
 
 const NotFound = () => {
 	return (
-		<div className='my-3 text-center'>
-			<p className='text-red-500'>Something went wrong!!!</p>
-			<p className='text-red-400'>Error Message</p>
-			<h4 className="text-3xl"> Please <button>Sign out</button> and log back in</h4>
+		<div className='h-screen '>
+			<Navbar />
+			<div className='my-3 text-center'>
+				<p className='text-red-500'>Something went wrong!!!</p>
+				<p className='text-red-400'>Error Message</p>
+				<h4 className="text-3xl"> Please <button>Sign out</button> and log back in</h4>
+			</div>
+			<Footer />
 		</div>
 	);
 };
