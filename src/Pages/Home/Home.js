54ce5141b4516adfb3d96c4add7bc0a238b28ac3
RAKIBUL <rakibulmd933@gmail.com>
2023-01-29 21:42:26 +0600

    [ADD] bacground photo aded

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import bg from '../../assets/pexels-daniel-reche-1556704.jpg';
 
 const Home = () => {
 	return (
-		<div>
+		<div style={{ background: `url(${bg})` }} className="min-h-screen bg-cover">
 			<h1>Home</h1>
 		</div>
 	);
