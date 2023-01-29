5b1344e2f57505778ccbf9914830232b719e6d73
RAKIBUL <rakibulmd933@gmail.com>
2023-01-29 23:00:13 +0600

    [ADD] home page finish

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import bg from '../../assets/pexels-daniel-reche-1556704.jpg';
 
 const Home = () => {
 	return (
-		<div style={{ background: `url(${bg})` }} className="min-h-screen bg-cover">
-			<h1>Home</h1>
+		<div style={{ background: `url(${bg})` }} className="relative min-h-screen bg-cover ">
+			<div>
+				<div className="absolute bottom-1/3 left-40">
+					<h3 className='my-4 text-xl font-normal text-white'>Using sunlight to prevent electricity waste.</h3>
+					<button className='p-2 text-base font-medium bg-white rounded w-36 hover:bg-blue-500'><Link to='/pay-bill'>Pay Bill</Link></button>
+				</div>
+			</div>
 		</div>
 	);
 };
