4a9580cf5f32bf09714b41b65836573260f55a9b
RAKIBUL <rakibulmd933@gmail.com>
2023-01-30 15:38:23 +0600

    [ADD] total number

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,8 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Loading from '../Loading/Loading';
 
 
 const Navbar = () => {
+	const { data, isLoading, error } = useQuery({
+		queryKey: ['length'],
+		queryFn: () =>
+			fetch('http://localhost:5000/bill-list').then(
+				(res) => res.json()
+			),
+	},
+		{
+			refetchInterval: 2000,
+		}
+	);
+
+	if (isLoading) {
+		return <Loading />
+	}
+	if (error) {
+		console.log(error);
+	}
 
 	return (
 		<div className="px-6 navbar bg-base-200">
@@ -10,7 +30,7 @@ const Navbar = () => {
 				<Link className="text-xl normal-case btn btn-ghost" to='/'>Power Hack</Link>
 			</div>
 			<div className="navbar-end">
-				<button>Paid Total: 0 </button>
+				<button>Paid Total: </button>
 			</div>
 		</div>
 	);
